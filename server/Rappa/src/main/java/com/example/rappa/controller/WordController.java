ae3ce1cd0fa44742a9a82c375c6a5492757bd6a5
Nhan <62550213+NhanPz01@users.noreply.github.com>
2024-12-21 04:56:24 +0700

    update server | rhyme finder method

---
+++ server/Rappa/src/main/java/com/example/rappa/controller/WordController.java
@@ -0,0 +1,37 @@
+package com.example.rappa.controller;
+
+import com.example.rappa.model.Word;
+import com.example.rappa.service.WordService;
+import jakarta.validation.constraints.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/word")
+public class WordController {
+    @Autowired
+    private WordService wordService;
+
+    //find all
+    @GetMapping("/all")
+    public List<Word> findAll() {
+        return wordService.findAll();
+    }
+
+    //find word by word
+    @GetMapping("/find/{word}")
+    public List<Word> findByWord(@PathVariable String word) {
+        return wordService.findByWord(word);
+    }
+
+    //find word with same rhyme, use url path to get input
+    @GetMapping("/rhyme/{word}")
+    public List<Word> getWordsWithSameRhyme(@PathVariable String word) {
+        return wordService.getWordsWithSameRhyme(word);
+    }
+}
---
+++ server/Rappa/src/main/java/com/example/rappa/service/impl/WordServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.rappa.service.impl;
+
+import com.example.rappa.model.Word;
+import com.example.rappa.repository.WordRepository;
+import com.example.rappa.service.WordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WordServiceImpl implements WordService {
+    @Autowired
+    private WordRepository wordRepository;
+
+    @Override
+    public List<Word> findAll() {
+        return wordRepository.findAll();
+    }
+
+    @Override
+    public List<Word> findByWord(String word) {
+        return wordRepository.findByWord(word);
+    }
+
+    @Override
+    public List<Word> getWordsWithSameRhyme(String word) {
+        // Make logic to find words with the same rhyme
+        //danh sách phụ âm
+        String[] consonants = {"ngh","nh","ng","gh","kh","ph","ch","b","c","d","đ","g","h","k","l","m","n","p","q","r","s","t","th","tr","v","x"};
+        // loại bỏ phụ âm ở đầu
+        for (String consonant : consonants) {
+            if (word.startsWith(consonant)) {
+                word = word.substring(consonant.length());
+                break;
+            }
+        }
+        return wordRepository.findAllByNoAccentEndingWith(word);
+    }
+
+}
